c22cf2d8af840899d18b37a6613cdfa4c5ed0067
SamDanielThangarajan <samdaniel.thangarajan@gmail.com>
2015-05-05 17:17:29 +0200

    Initial Project Setup

---
+++ plugin/revenge/src/org/samdanielt/plugin/handler/REnghandler.java
@@ -0,0 +1,22 @@
+package org.samdanielt.plugin.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandlerListener;
+
+public class REnghandler extends AbstractHandler {
+
+	@Override
+	public void addHandlerListener(IHandlerListener handlerListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
